2b11aa61398bcc3d8ac5aeb21b6d47dd9a90c256
Andrei <andrei.carare@artehnis.ro>
2022-05-16 16:55:55 +0300

    Revert "deleted git files from vue app"
    This reverts commit 99b4ceb974009ae2b05e88e613c85a533018fccc.

--- vue-django/django_rest_api/tasks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from tasks import views
-from rest_framework import routers
-router = routers.DefaultRouter()
-router.register(r'taskss', views.TaskViewSet)
-urlpatterns = [
-        path('api-auth/', include('rest_framework.urls')),
-        path('', include(router.urls)),
-]
